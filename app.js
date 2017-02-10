a5a61fe506fc4862618d969b5afed63c8b12770f
Kyle DM <confidential.inc@gmail.com>
2017-02-09 21:22:29 -0700

    changed structure (express)

--- app.js
+++ app.js
@@ -1,71 +1,46 @@
 var express = require('express');
-var http = require('http');
-var morgan = require('morgan');
-var compression = require('compression');
-var cookieSession = require('cookie-session');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var favicon = require('serve-favicon')
 
-var version = "0.1.0";
+var index = require('./routes/index');
+var users = require('./routes/users');
 
-// Main App
 var app = express();
 
-// View Engine
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Logging
-app.use(morgan('tiny'));
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// favicon
-//app.use(favicon(__dirname + '/assets/favicon.ico'));
+app.use('/', index);
+app.use('/users', users);
 
-// gzip/deflate outgoing responses
-app.use(compression());
-
-// store session state in browser cookie
-app.use(cookieSession({
-    keys: ['myD0llar1', 'myD0llar2']
-}));
-
-// parse urlencoded request bodies into req.body
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-
-// TODO
-app.use('/', (req, res) => {
-    res.render('index',{
-        myDollarVersion: version,
-        page: "index"
-    });
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// TODO
-app.use('/dashboard', (req, res) => {
-    res.render('dashboard');
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-// TODO
-app.use('/logout', (req, res) => {
-    res.render('logout');
-});
-
-// TODO
-app.use('/login', (req, res) => {
-    res.render('login');
-});
-
-// TODO
-app.use((req, res) => {
-    res.render('x40x');
-});
-
-// TODO
-app.use((req, res) => {
-    res.render('x50x');
-});
-
-
-//create node.js http server and listen on port
-http.createServer(app).listen(3000);
+module.exports = app;
